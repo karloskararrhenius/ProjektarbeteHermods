8561fd31387546a65f3e47013d27ab4a118ec9be
mcr8802 <teh.muzzi@gmail.com>
2016-04-28 22:56:54 +0200

    fixed some stuff hopefully

---
+++ ProjektHermods/Controllers/SearchController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Sql;
+using System.Web.Mvc;
+using ProjektHermods.Models;
+
+namespace ProjektHermods.Controllers
+    {
+    public class SearchController : Controller
+        {
+        // GET: Search
+        public ActionResult Index()
+            {
+
+
+            return View();
+            }
+
+        public ActionResult Testing()
+            {
+            //int identifier;
+            //var isInt = int.TryParse(Request.QueryString["id"], out identifier);
+            //Ingrediens context = new Ingrediens();
+            //if(context.Id ==identifier)
+            //    {
+            //    context.ChosenIngredient++;
+            //    }
+
+            return View();
+            }
+
+        public JsonResult AutoCompleteIngrediens(string term)
+            {
+            ReceptTipsContext context = new ReceptTipsContext();
+
+            var result = (from r in context.Ingrediens
+                          where r.Name.ToLower().StartsWith(term.ToLower())
+                          select new { r.Name }).Distinct();
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+
+            }
+        }
+    }
